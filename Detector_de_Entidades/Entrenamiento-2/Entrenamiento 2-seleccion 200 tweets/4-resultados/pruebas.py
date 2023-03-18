9a608cd15905204cf3eebdf7e9e3da3fc9f12f1d
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2023-03-18 18:28:09 -0300

    Agregar resultados con dataset expandido a 300 tweets, mas pruebas

--- Detector_de_Entidades/Entrenamiento-2/Entrenamiento 2-seleccion 200 tweets/4-resultados/pruebas.py
+++ Detector_de_Entidades/Entrenamiento-2/Entrenamiento 2-seleccion 200 tweets/4-resultados/pruebas.py
@@ -1,13 +1,13 @@
 import spacy
 
 # define path to custom-trained model
-model_path = "C:/Users/Valentina/Desktop/Proyecto_Final/Proyecto-Final/Detector_de_Entidades/Entrenamiento-2/Entrenamiento 2- frases + tweets/4-resultados/output/model-best"
+model_path = "/home/pablo/Documents/vs-workspace/Proyecto-Final/Detector_de_Entidades/Entrenamiento-2/Entrenamiento 2-seleccion 200 tweets/4-resultados/output-v2/model-best"
 
 # load the custom-trained model
 nlp = spacy.load(model_path)
 
 # sample text input
-text = "Juan Martinez nacio en Hawaii el 10 de Marzo de 1961."
+text = "Juan Martinez nacio en Hawaii el 10 de Marzo de 1961. Durante la decada del 80, empezó a aparecer en varias películas. En 1981, viajó a Argentina y fundó dos empresas. Un mes despues, en diciembre, chocó con su auto en la playa. Llamó a su amigo Luis para decirle que perdió la memoria. Cuando Luis le preguntó como recordaba su nombre, Juan se dio cuenta de que estaba bien. Le pagó 2000 pesos por ayudarlo."
 
 # process the text with spaCy
 doc = nlp(text)
