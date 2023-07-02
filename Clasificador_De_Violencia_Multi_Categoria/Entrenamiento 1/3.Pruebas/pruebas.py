08a2929e27ae0c96b917b2830a5a2b53e9c7a978
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2023-07-02 19:54:03 -0300

    Agregar entrenamiento 1 y entrenamiento 1 con transformers para clasificador de violencia multicategoria

---
+++ Clasificador_De_Violencia_Multi_Categoria/Entrenamiento 1/3.Pruebas/pruebas.py
@@ -0,0 +1,35 @@
+import spacy
+import os
+
+def prueba_txt(file):
+    """
+    Probar el modelo entrenado con un archivo de texto.
+
+    Parameters
+    ----------
+    :param: file (str)  Nombre del archivo de texto.
+
+    """
+    # define path to custom-trained model
+    model_path = os.path.join(os.getcwd(), "output", "model-best")
+
+    # load the custom-trained model
+    nlp = spacy.load(model_path)
+
+    # sample text input
+    with open(file, 'r') as file:
+        textList = file.read().split('\n')
+
+    # process the text with spaCy
+    for text in textList:
+        if "--" in text: # Para poder agregar comentarios con '--'
+            continue
+        doc = nlp(text)
+        print(text)
+        for key, value in doc.cats.items():
+            print(key, ":", round(value, 3), "- ", end="")
+
+        print("")
+        print("")
+
+prueba_txt('pruebas.txt')
